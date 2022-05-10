3b9b80de860441d29a1151d4df82e83d55108182
alec weinhofer <Alec.Weinhofer@ulb.be>
2022-05-10 21:31:34 +0000

    Added arguments and possibility to send files.

--- disquery.py
+++ disquery.py
@@ -1,25 +1,34 @@
 import discord
+import sys
+
 
 client = discord.Client()
 
 @client.event
 async def on_ready():
     print("Logged in as {0.user}".format(client))
+    channel = client.get_channel(int(channel_id))
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+    for i in range(1, len(sys.argv[1:]), 2):
 
-    print("Received a message : closing")
+        if sys.argv[i] == "-m":
+            await channel.send("got a -m ! : " + sys.argv[i+1])
+        elif sys.argv[i] == "-f":
+            await channel.send(file=discord.File(sys.argv[i+1]))
+        else:
+            print("wrong arg")
+
     await client.close()
-
 
 
 ########################################## MAIN ##########################################
 
 with open("discloud.conf", 'r') as f:
-    token = f.read()
+    token = f.readline()
+    channel_id = f.readline()
+
+
+
 
 client.run(token)
 
